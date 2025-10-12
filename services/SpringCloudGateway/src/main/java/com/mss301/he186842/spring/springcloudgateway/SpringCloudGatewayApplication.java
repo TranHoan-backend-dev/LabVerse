3540867652af1b9c50a11d78fa343eb60906c51f
dangthaison12 <thaison02004@gmail.com>
2025-10-12 18:03:43 +0700

    feat: config API gate way

---
+++ services/SpringCloudGateway/src/main/java/com/mss301/he186842/spring/springcloudgateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.mss301.he186842.spring.springcloudgateway;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway 1.0"))
+public class SpringCloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+
+}
---
+++ services/SpringCloudGateway/src/test/java/com/mss301/he186842/spring/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mss301.he186842.spring.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
