2371aa11872ba0d945bbeac7416affb5ea703ee1
TranHoan2004 <hoana5k44nknd@gmail.com>
2025-10-19 12:04:03 +0700

    feat: add module handling pdf parsing

--- services/paper-service/src/main/kotlin/com/se1853_jv/controller/PaperController.kt
+++ services/paper-service/src/main/kotlin/com/se1853_jv/controller/PaperController.kt
@@ -2,14 +2,19 @@ package com.se1853_jv.controller
 
 import com.se1853_jv.dto.response.WrapperApiResponse
 import com.se1853_jv.service.EncoderService
+import com.se1853_jv.service.GrobidService
 import com.se1853_jv.service.boundary.CitationService
 import com.se1853_jv.service.boundary.PaperService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+import java.io.File
 import java.time.LocalDateTime
 
 @RestController
@@ -17,7 +22,8 @@ import java.time.LocalDateTime
 class PaperController(
     private val paperService: PaperService,
     private val citationService: CitationService,
-    private val encoder: EncoderService
+    private val encoder: EncoderService,
+    private val grobid: GrobidService
 ) {
     @GetMapping("/details")
     fun getDetails(@RequestParam("id") data: String): ResponseEntity<WrapperApiResponse> {
@@ -41,6 +47,24 @@ class PaperController(
                 HttpStatus.OK.value(),
                 "Get citations of a paper successfully",
                 citationService.getCitationsByPaperId(id),
+                LocalDateTime.now()
+            )
+        )
+    }
+
+    @PostMapping("/pdf/parse", produces = ["application/json"], consumes = ["multipart/form-data"])
+    fun extractDataFromPdfFile(@RequestParam("file") file: MultipartFile): ResponseEntity<WrapperApiResponse> {
+        val temp: File = File.createTempFile("pdf_", ".pdf")
+        file.transferTo(temp)
+
+        val metadata: MutableList<MutableMap<String, String>> = grobid.parsePdf(temp)
+        temp.delete()
+
+        return ResponseEntity.ok(
+            WrapperApiResponse(
+                HttpStatus.OK.value(),
+                "Parse citations of a paper successfully",
+                metadata,
                 LocalDateTime.now()
             )
         )
