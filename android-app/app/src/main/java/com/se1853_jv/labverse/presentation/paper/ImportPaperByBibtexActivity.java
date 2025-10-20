fb10be1677044c4042c29c046dd54b4a4bb87c84
TranHoan2004 <hoana5k44nknd@gmail.com>
2025-10-20 22:07:59 +0700

    refactor: apply the WindowInsets for layout

---
+++ android-app/app/src/main/java/com/se1853_jv/labverse/presentation/paper/ImportPaperByBibtexActivity.java
@@ -0,0 +1,15 @@
+package com.se1853_jv.labverse.presentation.paper;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.se1853_jv.labverse.R;
+
+public class ImportPaperByBibtexActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_import_bibtex);
+    }
+}
