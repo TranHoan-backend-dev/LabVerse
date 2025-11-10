083ca89fc3e6006f68598f203314c05fc6a5c732
TranHoan2004 <hoana5k44nknd@gmail.com>
2025-11-10 11:33:46 +0700

    fix: fix method requests

--- web/src/type/constant.ts
+++ web/src/type/constant.ts
@@ -2,4 +2,11 @@ export const BASE_API_URL = "http://localhost:8080"
 export const PAPER_SERVICE_PREDICATE = "paper-service"
 export const GROUP_SERVICE_PREDICATE = "group-service"
 
-export enum METHOD {"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"}
+export enum METHOD {
+    GET = "GET",
+    POST = "POST",
+    PUT = "PUT",
+    PATCH = "PATCH",
+    DELETE = "DELETE",
+    HEAD = "HEAD"
+}
